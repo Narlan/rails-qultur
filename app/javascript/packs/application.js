c786670d4b4ebd85a927807396b8ca0877201ede
Adam Castro Guzman <castroguzman033@gmail.com>
2019-08-20 18:40:31 +0200

    Qr code reader works, but the view need a script migration into the right place with a propper code revriting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,14 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
-import { openQRCamera } from "./scan.js"
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 
 initMapbox();
-openQRCamera()
+
+// Il semble qu'il fasse ajouter des listener pour pouvoir utiliser cette structure...
+// Le script du lecteur qr est actuellement dans la view...
+// import { openQRCamera } from "./scan.js"
+// import { showQRIntro } from "./scan.js"
+// openQRCamera();
+// showQRIntro();
