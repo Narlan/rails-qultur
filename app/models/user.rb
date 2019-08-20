3392de83dcc47f0094710b4817032f235c245352
Jonathan Wagner <jonathan.wagner88@gmail.com>
2019-08-20 13:44:20 +0200

    changed navbar top and sign forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :hunts
+  mount_uploader :photo, PhotoUploader
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
