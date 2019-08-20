5b69185163aecdd9279e9149079c389bedd40a50
julien legay <legayj@gmail.com>
2019-08-20 15:08:23 +0200

    map 3D

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -22,6 +22,47 @@ const initMapbox = () => {
       antialias: true
     });
 
+    // The 'building' layer in the mapbox-streets vector source contains building-height
+    // data from OpenStreetMap.
+    map.on('load', function() {
+    // Insert the layer beneath any symbol layer.
+    var layers = map.getStyle().layers;
+
+    var labelLayerId;
+    for (var i = 0; i < layers.length; i++) {
+      if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+        labelLayerId = layers[i].id;
+        break;
+      }
+    }
+
+    map.addLayer({
+      'id': '3d-buildings',
+      'source': 'composite',
+      'source-layer': 'building',
+      'filter': ['==', 'extrude', 'true'],
+      'type': 'fill-extrusion',
+      'minzoom': 15,
+      'paint': {
+      'fill-extrusion-color': '#aaa',
+
+      // use an 'interpolate' expression to add a smooth transition effect to the
+      // buildings as the user zooms in
+      'fill-extrusion-height': [
+      "interpolate", ["linear"], ["zoom"],
+      15, 0,
+      15.05, ["get", "height"]
+      ],
+      'fill-extrusion-base': [
+      "interpolate", ["linear"], ["zoom"],
+      15, 0,
+      15.05, ["get", "min_height"]
+      ],
+      'fill-extrusion-opacity': .6
+    }
+    }, labelLayerId);
+    });
+
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
       new mapboxgl.Marker()
@@ -31,5 +72,7 @@ const initMapbox = () => {
     fitMapToMarkers(map, markers);
   }
 };
+
+
 
 export { initMapbox };
