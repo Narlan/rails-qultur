06b5ebb0dfe93222eaf082667ab0ae329e404cf6
julien legay <legayj@gmail.com>
2019-08-21 18:15:37 +0200

    delete scan

--- test/controllers/scan_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ScanControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/scans_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ScansControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
