b411c706e1b15e8e6c98f1ad3d0f0ea532d5f37f
Adam Castro Guzman <castroguzman033@gmail.com>
2019-08-21 19:03:34 +0200

    trsting

--- app/javascript/packs/scan.js
+++ app/javascript/packs/scan.js
@@ -1,30 +1,30 @@
-// const qrCodeActivated = () => {
-//   const to_send = document.getElementById("field-to-send")
-//   const field = document.getElementById("qr-field");
-//   field.addEventListener("change", (node) => {
+const qrCodeActivated = () => {
+  const to_send = document.getElementById("field-to-send")
+  const field = document.getElementById("qr-field");
+  field.addEventListener("change", (node) => {
 
-//     console.log(field.value);
-//     console.log(field);
-//     const reader = new FileReader();
-//     reader.onload = function() {
-//       field.value = "";
-//       qrcode.callback = function(res) {
-//         if(res instanceof Error) {
-//           console.log("no");
-//           alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.");
-//         } else {
-//           console.log("yes");
-//           field.parentNode.previousElementSibling.value = res;
-//           to_send.value = res;
-//           document.querySelectorAll(".qr-code-form")[0].submit();
-//         };
-//       };
-//       qrcode.decode(reader.result);
-//     };
-//     reader.readAsDataURL(field.files[0]);
+    console.log(field.value);
+    console.log(field);
+    const reader = new FileReader();
+    reader.onload = function() {
+      field.value = "";
+      qrcode.callback = function(res) {
+        if(res instanceof Error) {
+          console.log("no");
+          alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.");
+        } else {
+          console.log("yes");
+          field.parentNode.previousElementSibling.value = res;
+          to_send.value = res;
+          document.querySelectorAll(".qr-code-form")[0].submit();
+        };
+      };
+      qrcode.decode(reader.result);
+    };
+    reader.readAsDataURL(field.files[0]);
 
-//   });
-// }
+  });
+}
 
-// export { qrCodeActivated };
+export { qrCodeActivated };
 
