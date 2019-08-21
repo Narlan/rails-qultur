a5907fa5601dcae97cbd45150f5197d1f20391bc
Florian Kogon <F.Kogon@gmail.com>
2019-08-21 18:10:06 +0200

    tiens julien

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@ import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete.js';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
-
 initMapbox();
 initAutocomplete();
 
