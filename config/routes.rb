fd4d0616eb7511cd3f89c523ea3f4f026d68827f
julien legay <legayj@gmail.com>
2019-08-21 16:19:40 +0200

    map pop up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only: [:index, :show]
-  resources :scans, only: [:index]
   resources :monuments, only: [:index, :show] do
     resources :questions, only: [:index, :new, :create]
   end
