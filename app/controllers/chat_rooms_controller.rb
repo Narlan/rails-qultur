5d733d6537f5dba211abfb1ddd6b61713da2955f
Jonathan Wagner <jonathan.wagner88@gmail.com>
2019-08-22 18:41:20 +0200

    done chat

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,7 +1,7 @@
 class ChatRoomsController < ApplicationController
 
   def index
-    @chat_rooms = ChatRoom.all
+    @chat_rooms = ChatRoom.where(params[:id] == current_user.id)
   end
 
   def show
@@ -9,28 +9,25 @@ class ChatRoomsController < ApplicationController
   end
 
   def new
-    @users = []
-    User.all.each do |user|
-      @users << user.nickname
-    end
+    @users = User.all
     @chat_room = ChatRoom.new
   end
 
   def create
     @chat_room = ChatRoom.new
-    @chat_room.user = User.find(params[:query][:name])
+    @chat_room.user = User.find(params[:query][:user_id])
     if current_user == @user
-      @chat_room.name = "Chat with #{@current_user.first_name}"
+      @chat_room.name = "Chat with #{@chat_room.user.nickname}"
     else
-      @chat_room.name = "Chat with #{@current_user.first_name}"
+      @chat_room.name = "Chat with #{@chat_room.user.nickname}"
     end
     @chat_room.save
     redirect_to "/users/#{current_user.id}/chat_rooms/#{@chat_room.id}"
   end
 
-  # private
-
-  # def params_chatroom
-  #   params.require(:chat_room).permit(:name)
-  # end
+  def destroy
+    @chat_room = ChatRoom.find(params[:id])
+    @chat_room.destroy
+    redirect_to user_chat_rooms_path(current_user)
+  end
 end
