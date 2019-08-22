87dc8f466fc88af2873dedcbda535dd17c3eab23
Adam Castro Guzman <castroguzman033@gmail.com>
2019-08-22 08:44:14 +0200

    Fixed

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -2,7 +2,7 @@ class Monument < ApplicationRecord
   CITY = ["Bordeaux", "Paris", "Lille", "Lyon", "Marseille", "Toulouse", "Dijon",
           "Strasbourg", "Brest", "Montpellier"].sort
 
-  geocoded_by :city
+  geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
   has_many :questions
