e760589eeca7b7841cd49d0bdd8e4b17b3943717
julien legay <legayj@gmail.com>
2019-08-22 13:35:47 +0200

    update position tower

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -20,7 +20,7 @@ const initMapbox = () => {
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     console.log("Detected: map div")
-    mapboxgl.accessToken = 'pk.eyJ1Ijoia2VzaHJhIiwiYSI6ImNqempxeTFsNTBidjIzbXBtbnE0YTVudTUifQ.qHqQ3V2uosWobIRloC68ag';
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     var map = window.map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v10',
@@ -33,81 +33,87 @@ const initMapbox = () => {
     // parameters to ensure the model is georeferenced correctly on the map
     var modelOrigin = [2.294481, 48.858370];
     var modelAltitude = 0;
-    var modelRotate = [Math.PI / 2, 180, 0];
-    var modelScale = 550.41843220338983e-8;
+    var modelRotate = [Math.PI / 2, 179.9, 0];
+    var modelScale = 530.41843220338983e-8;
 
     // transformation parameters to position, rotate and scale the 3D model onto the map
     var modelTransform = {
-    translateX: mapboxgl.MercatorCoordinate.fromLngLat(modelOrigin, modelAltitude).x,
-    translateY: mapboxgl.MercatorCoordinate.fromLngLat(modelOrigin, modelAltitude).y,
-    translateZ: mapboxgl.MercatorCoordinate.fromLngLat(modelOrigin, modelAltitude).z,
-    rotateX: modelRotate[0],
-    rotateY: modelRotate[1],
-    rotateZ: modelRotate[2],
-    scale: modelScale
+      translateX: mapboxgl.MercatorCoordinate.fromLngLat(modelOrigin, modelAltitude).x,
+      translateY: mapboxgl.MercatorCoordinate.fromLngLat(modelOrigin, modelAltitude).y,
+      translateZ: mapboxgl.MercatorCoordinate.fromLngLat(modelOrigin, modelAltitude).z,
+      rotateX: modelRotate[0],
+      rotateY: modelRotate[1],
+      rotateZ: modelRotate[2],
+      scale: modelScale
     };
 
     var THREE = window.THREE;
 
     // configuration of the custom layer for a 3D model per the CustomLayerInterface
     var customLayer = {
-    id: '3d-model',
-    type: 'custom',
-    renderingMode: '3d',
-    onAdd: function(map, gl) {
-    this.camera = new THREE.Camera();
-    this.scene = new THREE.Scene();
+      id: '3d-model',
+      type: 'custom',
+      renderingMode: '3d',
+      onAdd: function(map, gl) {
+      this.camera = new THREE.Camera();
+      this.scene = new THREE.Scene();
 
-    // create two three.js lights to illuminate the model
-    var directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(0, -70, 100).normalize();
-    this.scene.add(directionalLight);
+      // create two three.js lights to illuminate the model
+      var directionalLight = new THREE.DirectionalLight(0xffffff);
+      directionalLight.position.set(0, -70, 100).normalize();
+      this.scene.add(directionalLight);
 
-    var directionalLight2 = new THREE.DirectionalLight(0xffffff);
-    directionalLight2.position.set(0, 70, 100).normalize();
-    this.scene.add(directionalLight2);
+      var directionalLight2 = new THREE.DirectionalLight(0xffffff);
+      directionalLight2.position.set(0, 70, 100).normalize();
+      this.scene.add(directionalLight2);
 
-    // use the three.js GLTF loader to add the 3D model to the three.js scene
-    var loader = new THREE.GLTFLoader();
-    loader.load('https://raw.githubusercontent.com/Narlan/rails-qultur/master/public/gltfs/eiffel.gltf', (function (gltf) {
-    this.scene.add(gltf.scene);
-    console.dir(gltf.scene);
-    }).bind(this));
-    this.map = map;
-    console.log(loader);
+      // use the three.js GLTF loader to add the 3D model to the three.js scene
+      var loader = new THREE.GLTFLoader();
+      loader.load('https://raw.githubusercontent.com/Narlan/rails-qultur/master/public/gltfs/eiffel.gltf', (function (gltf) {
+      this.scene.add(gltf.scene);
+      }).bind(this));
+      this.map = map;
+      console.log(loader);
 
-    // use the Mapbox GL JS map canvas for three.js
-    this.renderer = new THREE.WebGLRenderer({
-    canvas: map.getCanvas(),
-    context: gl,
-    antialias: true
+      // use the Mapbox GL JS map canvas for three.js
+      this.renderer = new THREE.WebGLRenderer({
+      canvas: map.getCanvas(),
+      context: gl,
+      antialias: true
     });
 
     this.renderer.autoClear = false;
     },
     render: function(gl, matrix) {
-    var rotationX = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(1, 0, 0), modelTransform.rotateX);
-    var rotationY = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 1, 0), modelTransform.rotateY);
-    var rotationZ = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 0, 1), modelTransform.rotateZ);
+      var rotationX = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(1, 0, 0), modelTransform.rotateX);
+      var rotationY = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 1, 0), modelTransform.rotateY);
+      var rotationZ = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 0, 1), modelTransform.rotateZ);
 
-    var m = new THREE.Matrix4().fromArray(matrix);
-    var l = new THREE.Matrix4().makeTranslation(modelTransform.translateX, modelTransform.translateY, modelTransform.translateZ)
-    .scale(new THREE.Vector3(modelTransform.scale, -modelTransform.scale, modelTransform.scale))
-    .multiply(rotationX)
-    .multiply(rotationY)
-    .multiply(rotationZ);
+      var m = new THREE.Matrix4().fromArray(matrix);
+      var l = new THREE.Matrix4().makeTranslation(modelTransform.translateX, modelTransform.translateY, modelTransform.translateZ)
+      .scale(new THREE.Vector3(modelTransform.scale, -modelTransform.scale, modelTransform.scale))
+      .multiply(rotationX)
+      .multiply(rotationY)
+      .multiply(rotationZ);
 
-    this.camera.projectionMatrix.elements = matrix;
-    this.camera.projectionMatrix = m.multiply(l);
-    this.renderer.state.reset();
-    this.renderer.render(this.scene, this.camera);
-    this.map.triggerRepaint();
+      this.camera.projectionMatrix.elements = matrix;
+      this.camera.projectionMatrix = m.multiply(l);
+      this.renderer.state.reset();
+      this.renderer.render(this.scene, this.camera);
+      this.map.triggerRepaint();
     }
     };
 
     map.on('style.load', function() {
     map.addLayer(customLayer, 'waterway-label');
     });
+
+    map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+    enableHighAccuracy: true
+  },
+    trackUserLocation: true
+  }));
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
@@ -120,5 +126,7 @@ const initMapbox = () => {
     console.log("Not detected: map div")
   }
 };
+
+
 
 export { initMapbox };
